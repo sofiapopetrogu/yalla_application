feecd0194142fc1a1e3631b67b72a73c044a3e44
SOFIAPOPETROGU <sofiapope.trogu@studenti.unipd.it>
2023-05-17 12:16:36 +0200

    changed login page

---
+++ lib/util/impact.dart
@@ -0,0 +1,12 @@
+class Impact{
+
+  static String baseUrl = 'https://impact.dei.unipd.it/bwthw/';
+  static String pingEndpoint = 'gate/v1/ping/';
+  static String tokenEndpoint = 'gate/v1/token/';
+  static String refreshEndpoint = 'gate/v1/refresh/';
+  static String stepEndpoint = 'data/v1/steps/';
+  
+  static String username = 'XfGoBpZk5e';
+  static String password = '12345678!';
+
+}//Impact
