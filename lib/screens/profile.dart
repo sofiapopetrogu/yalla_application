5b949fda37e4c20a198426876c3005f4baf6f710
SOFIAPOPETROGU <sofiapope.trogu@studenti.unipd.it>
2023-06-01 18:07:42 +0200

    updated Profile page class

--- lib/screens/profile.dart
+++ lib/screens/profile.dart
@@ -2,6 +2,10 @@ import 'package:flutter/material.dart';
 import 'package:material_design_icons_flutter/material_design_icons_flutter.dart';
 
 class ProfilePage extends StatelessWidget {
+  ProfilePage({Key? key}) : super(key: key);
+
+  static const routename = 'ProfilePage';
+  
   @override
   Widget build(BuildContext context) {
     return Scaffold(
