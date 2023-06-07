239d154bec637198c064750eb6d3af5591b8d68d
SOFIAPOPETROGU <sofiapope.trogu@studenti.unipd.it>
2023-06-07 12:17:02 +0200

    added heart and resting endpoints

--- lib/util/authentication.dart
+++ lib/util/authentication.dart
@@ -7,3 +7,70 @@ import 'package:http/http.dart' as http;
 import 'package:jwt_decoder/jwt_decoder.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 
+class Authentication {
+
+//This method allows to check if the IMPACT backend is up
+  Future<bool> _isImpactUp() async {
+
+    //Create the request
+    final url = Impact.baseUrl + Impact.pingEndpoint;
+
+    //Get the response
+    print('Calling: $url');
+    final response = await http.get(Uri.parse(url));
+
+    //Just return if the status code is OK
+    return response.statusCode == 200;
+  } //_isImpactUp
+
+  //This method allows to obtain the JWT token pair from IMPACT and store it in SharedPreferences
+  Future<int> _getAndStoreTokens() async {
+
+    //Create the request
+    final url = Impact.baseUrl + Impact.tokenEndpoint;
+    final body = {'username': Impact.username, 'password': Impact.password};
+
+    //Get the response
+    print('Calling: $url');
+    final response = await http.post(Uri.parse(url), body: body);
+
+    //If response is OK, decode it and store the tokens. Otherwise do nothing.
+    if (response.statusCode == 200) {
+      final decodedResponse = jsonDecode(response.body);
+      final sp = await SharedPreferences.getInstance();
+      await sp.setString('access', decodedResponse['access']);
+      await sp.setString('refresh', decodedResponse['refresh']);
+    } //if
+
+    //Just return the status code
+    return response.statusCode;
+  } //_getAndStoreTokens
+
+  //This method allows to refrsh the stored JWT in SharedPreferences
+  Future<int> _refreshTokens() async {
+
+    //Create the request 
+    final url = Impact.baseUrl + Impact.refreshEndpoint;
+    final sp = await SharedPreferences.getInstance();
+    final refresh = sp.getString('refresh');
+    final body = {'refresh': refresh};
+
+    //Get the response
+    print('Calling: $url');
+    final response = await http.post(Uri.parse(url), body: body);
+
+    //If the response is OK, set the tokens in SharedPreferences to the new values
+    if (response.statusCode == 200) {
+      final decodedResponse = jsonDecode(response.body);
+      final sp = await SharedPreferences.getInstance();
+      await sp.setString('access', decodedResponse['access']);
+      await sp.setString('refresh', decodedResponse['refresh']);
+    } //if
+
+    //Just return the status code
+    return response.statusCode;
+    
+  } //_refreshTokens
+
+
+}
--- lib/util/impact.dart
+++ lib/util/impact.dart
@@ -5,7 +5,9 @@ class Impact{
   static String tokenEndpoint = 'gate/v1/token/';
   static String refreshEndpoint = 'gate/v1/refresh/';
   static String stepEndpoint = 'data/v1/steps/';
-  
+  static String heartEndpoint = 'data/v1/heart_rate/';
+  static String restingheartEndpoint = 'data/v1/resting_heart_rate/';
+
   static String username = 'XfGoBpZk5e';
   static String password = '12345678!';
 
