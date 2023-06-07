fb800174baa7a78dca5f3fec8cbc16fbf040ca02
SOFIAPOPETROGU <sofiapope.trogu@studenti.unipd.it>
2023-06-07 11:55:48 +0200

    added authentication page

---
+++ lib/util/authentication.dart
@@ -0,0 +1,9 @@
+import 'dart:convert';
+import 'dart:io';
+
+import 'package:flutter/material.dart';
+import 'package:project_app/util/impact.dart';
+import 'package:http/http.dart' as http;
+import 'package:jwt_decoder/jwt_decoder.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+
