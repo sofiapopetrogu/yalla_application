ef94ce2f5f4937dec1e2602bc03c6d946536538c
Nourhousseinii <nour.alhousseini@studenti.unipd.it>
2023-06-08 18:30:09 +0200

    added community hub data

--- lib/screens/community.dart
+++ lib/screens/community.dart
@@ -25,15 +25,15 @@ class CommunityHub extends StatelessWidget {
               title: Text('Previous User Experiences'),
               leading: Icon(Icons.history),
               children: <Widget>[
-                  ListTile(title: Text('UX 1'),
+                  ListTile(title: Text('Used this app several times before. It is very helpful. I would recommend it to anyone :).'),
                             shape: Border(
                             top: BorderSide(width: 1.0, color: Colors.grey),
                           ), ),
-                  ListTile(title: Text('UX 2'),
+                  ListTile(title: Text('At first, I disliked the app. However, after using it for a while, I have grown to like it.'),
                             shape: Border(
                             top: BorderSide(width: 1.0, color: Colors.grey),
                           ), ),
-                  ListTile(title: Text('UX 3'),
+                  ListTile(title: Text('Takes some time to get used to, because there are other applications that work the same. But i enjoyed the uniqueness of the analysis part of the app.'),
                             shape: Border(
                             top: BorderSide(width: 1.0, color: Colors.grey),
                           ), ),
@@ -48,15 +48,15 @@ class CommunityHub extends StatelessWidget {
               subtitle: Text('Ask a question or find answers to health-related queries.'), //TODO: update font size later
               leading: Icon(Icons.question_answer),
               children: <Widget>[
-                  ListTile(title: Text('QA 1'),
+                  ListTile(title: Text('My father has a heart condition. Is it hereditary? And should we both use the app?'),
                             shape: Border(
                             top: BorderSide(width: 1.0, color: Colors.grey),
                           ), ),
-                  ListTile(title: Text('QA 2'),
+                  ListTile(title: Text('Can my heart rate be too low?'),
                             shape: Border(
                             top: BorderSide(width: 1.0, color: Colors.grey),
                           ), ),
-                  ListTile(title: Text('QA 3'),
+                  ListTile(title: Text('Does the app calculate the different heart rate zones?'),
                             shape: Border(
                             top: BorderSide(width: 1.0, color: Colors.grey),
                           ), ),
@@ -70,15 +70,15 @@ class CommunityHub extends StatelessWidget {
               title: Text('User Reviews'),
               leading: Icon(Icons.group),
               children: <Widget>[
-                  ListTile(title: Text('UX 1'),
+                  ListTile(title: Text('I recommended this app to my brother who has arrythmia. He found it very useful !! 4/5.'),
                             shape: Border(
                             top: BorderSide(width: 1.0, color: Colors.grey),
                           ), ),
-                  ListTile(title: Text('UX 2'),
+                  ListTile(title: Text('Too much information. I would prefer if the app was more simple 2/5.'),
                             shape: Border(
                             top: BorderSide(width: 1.0, color: Colors.grey),
                           ), ),
-                  ListTile(title: Text('UX 3'),
+                  ListTile(title: Text('Nice. Easy. Simple. 5/5.'),
                             shape: Border(
                             top: BorderSide(width: 1.0, color: Colors.grey),
                           ), ),
