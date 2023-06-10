93e0abd1798b5a126730dcfbc70d9a0b4ea0cdaa
SOFIAPOPETROGU <sofiapope.trogu@studenti.unipd.it>
2023-06-10 16:49:57 +0200

    added step_db file

---
+++ lib/databases/step_db.dart
@@ -0,0 +1,16 @@
+//Imports that are necessary to the code generation of floor
+import 'dart:async';
+import 'package:floor/floor.dart';
+import 'package:sqflite/sqflite.dart' as sqflite;
+
+//Import the entities and daos of the database
+import 'package:project_app/databases/stepdao.dart';
+import 'package:project_app/databases/step_table.dart';
+
+part 'step_db.g.dart'; 
+
+@Database(version: 1, entities: [Steps_Daily])
+abstract class AppDatabase extends FloorDatabase {
+  StepDao get stepDao;
+}
+
